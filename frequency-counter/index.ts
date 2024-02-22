3f01b1ee0fd1a1b2ddde450c48a268d0a7422ce5
Deva92dev <buildwith92deva@gmail.com>
2024-02-23 01:22:48 +0530

    Started Learning DSA

---
+++ frequency-counter/index.ts
@@ -0,0 +1,9 @@
+// Compile TypeScript to JavaScript:
+// Make sure you have compiled your TypeScript code into JavaScript. If you haven't done this yet, you can use the TypeScript compiler (tsc) to do so. Run the following command in your terminal
+//npx tsc yourFileName.ts
+// Replace yourFileName.ts with the actual name of your TypeScript file.
+
+// Run the JavaScript file:
+// After compiling, you should have a corresponding JavaScript file (e.g., yourFileName.js). Run this file using Node.js:
+// node yourFileName.js
+// This command will execute the JavaScript file and should display the result in the terminal
